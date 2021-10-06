a22a15d4c35f0dbe596c87a5fd5626a46bb87496
monaboiste <monikaszumna4@gmail.com>
2021-10-06 22:34:58 +0200

    Splitted application into modules

---
+++ food-order-domain/src/main/java/com/github/monaboiste/domain/order/port/outgoing/FoodOrderDatabase.java
@@ -0,0 +1,14 @@
+package com.github.monaboiste.domain.order.port.outgoing;
+
+import com.github.monaboiste.domain.order.port.shared.FoodOrderDto;
+import com.github.monaboiste.domain.order.port.shared.FoodOrderState;
+
+import java.util.Collection;
+
+public interface FoodOrderDatabase {
+
+    void removeAll();
+    void save(FoodOrderDto foodOrderDto);
+    FoodOrderDto findById(Long orderId);
+    Collection<FoodOrderDto> findByState(FoodOrderState state);
+}
